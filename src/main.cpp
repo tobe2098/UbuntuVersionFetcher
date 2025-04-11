ed161d41eb2b85fc4372461ae0fe39180fdf945f
Antoni Bertolin <tonibm1220@gmail.com>
2025-04-11 12:48:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,8 @@
 
-#include <iostream>
 #include "UbuntuCloudFactory.hpp"
+#include "handlers.hpp"
 
-constexpr int base_indentation { 1 };
-
-std::string indentation(int indentation_level) {
-  return std::string(2 * (indentation_level + base_indentation), ' ');
-}
-
-int printSupportedReleases(std::unique_ptr<UbuntuCloudInterface> fetcher) {
-  std::vector<UbuntuRelease> releases { fetcher->getSupportedReleases() };
-  if (releases.empty()) {
-    return 1;
-  }
-  bool LTS { false };
-  std::cout << '\n';
-  std::cout << indentation(0) << "Supported Ubuntu releases:\n";
-  std::cout << indentation(1) << "Non-LTS:\n";
-  for (auto& [release_name, archs] : releases) {
-    if (!LTS && release_name.find("LTS") != std::string::npos) {
-      LTS = true;
-      std::cout << indentation(1) << "LTS:\n";
-    }
-    if (LTS) {
-      size_t pos = release_name.find("LTS");
-      if (pos != std::string::npos) {
-        release_name.erase(pos, 4);
-      }
-    }
-    std::cout << indentation(2) << release_name << " : ";
-    for (const std::string& arch : archs) {
-      std::cout << arch << " ";
-    }
-    std::cout << "\n";
-  }
-  return 0;
-}
-
-void printHelp() {
-  std::cout << "Usage: ubuntu-version-fetcher [OPTION]\n"
-            << "Options:\n"
-            << "  --supported-releases     List all supported Ubuntu releases\n"
-            << "  --current-lts            Show the current Ubuntu LTS version\n"
-            << "  --sha256 RELEASE         Get the SHA256 hash of disk1.img for the specified release\n"
-            // << "  --url URL           Use custom Simplestreams URL\n"
-            << "  --help                   Display this help and exit\n";
-}
+#include <iostream>
 
 int main(int argc, char* argv[]) {
   std::ios_base::sync_with_stdio(false);
@@ -75,6 +32,7 @@ int main(int argc, char* argv[]) {
       std::cerr << "No supported Ubuntu releases were found.\n";
     }
   } else if (option == "--lts-version") {
+    return_code = printCurrentLTSRelease(std::move(fetcher));
   } else if (option == "--help") {
     printHelp();
   } else {
